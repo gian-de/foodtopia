e8a7cf4417fe5248eba95813da79d4c3a44b0ba5
giandemirjaen <giande.jaen@gmail.com>
2025-02-15 13:37:34 -0600

    added transaction in AccountService when deleting a User to delete both Rating & HeartedRecipe, not needed since cascade-delete in AppDbContext, but still

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -22,23 +22,41 @@ namespace foodtopia.Services
             var user = await _userManager.FindByIdAsync(userId.ToString());
             if (user is null) throw new KeyNotFoundException("User not found.");
 
-            // Grab Recipes created from User, then reassign UserId to DeleteGuid id 
-            var userRecipes = await _context.Recipes
-                                .Where(r => r.UserId == userId)
-                                .ToListAsync();
-            foreach (var recipe in userRecipes)
+            using var transaction = await _context.Database.BeginTransactionAsync();
+
+            try
             {
-                recipe.UserId = deletedUserGuid;
+                // Grab Recipes created from User, then reassign UserId to DeleteGuid id 
+                var userRecipes = await _context.Recipes
+                                    .Where(r => r.UserId == userId)
+                                    .ToListAsync();
+                foreach (var recipe in userRecipes)
+                {
+                    recipe.UserId = deletedUserGuid;
+                }
+
+                var userHeartedRecipes = await _context.HeartedRecipes
+                                            .Where(r => r.UserId == userId)
+                                            .ToListAsync();
+                _context.HeartedRecipes.RemoveRange(userHeartedRecipes);
+
+                var userRatings = await _context.Ratings
+                                    .Where(r => r.UserId == userId)
+                                    .ToListAsync();
+                _context.Ratings.RemoveRange(userRatings);
+
+                await _context.SaveChangesAsync();
+
+                var deleteUserResult = await _userManager.DeleteAsync(user);
+                if (!deleteUserResult.Succeeded) throw new Exception("Failed to delete account");
+
+                await transaction.CommitAsync();
             }
-
-            var userHeartedRecipes = await _context.HeartedRecipes
-                                        .Where(r => r.UserId == userId)
-                                        .ToListAsync();
-            _context.HeartedRecipes.RemoveRange(userHeartedRecipes);
-            await _context.SaveChangesAsync();
-
-            var deleteUserResult = await _userManager.DeleteAsync(user);
-            if (!deleteUserResult.Succeeded) throw new Exception("Failed to delete account");
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
         }
     }
 }
